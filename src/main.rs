c67adf4febbeaf4487aa112dd4a510235c98d66f
Marcus Kazmierczak <marcus@mkaz.com>
2021-02-03 21:20:50 -0800

    Add filename generation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 // extern crate toml;
 
+use chrono::{DateTime, Utc};
 use clap::{App, Arg};
-// use std::fs;
+use std::fs;
+use std::path::Path;
 
 use atty::Stream;
 use std::io::{self, Read};
@@ -19,42 +21,47 @@ fn main() {
                 .takes_value(true),
         )
         .arg(
-            Arg::new("digits")
-                .about("Include numbers")
-                .short('d')
-                .long("digits"),
-        )
-        .arg(
-            Arg::new("stdin")
-                .about("From stdin")
-                .short('s')
-                .long("stdin"),
+            Arg::new("config")
+                .about("Configuration file")
+                .short('c')
+                .long("config"),
         )
         .get_matches();
 
-    let notes_dir = "/home/mkaz/Documents/Notes/Zk";
+    let notes_path = Path::new("/home/mkaz/Documents/Notes/Zk");
     // let config_data = fs::read_to_string(config_file).unwrap();
     // let config = config_data.parse::<toml::Value>().unwrap();
 
     // check for command line args
     // check for nothing
     // check for stdin
+    let mut content = String::new();
 
     if is_pipe() {
-        let mut buffer = String::new();
         let mut stdin = io::stdin(); // We get `Stdin` here.
-        match stdin.read_to_string(&mut buffer) {
-            Ok(_) => println!("Read to buffer"),
-            Err(_) => println!("An error happened"),
+        match stdin.read_to_string(&mut content) {
+            Ok(_) => {}
+            Err(e) => println!("Error reading stdin: {:?}", e),
         };
-        println!("Buffer: {}", buffer);
     } else {
+        // read args from command-line
         println!("Not piped");
     }
 
-    println!("Done");
+    // get new filename
+    if content != "" {
+        let filename = get_new_filename();
+        let file_path = notes_path.join(filename);
+        println!("Creating file: {:?}", file_path);
+    }
 }
 
 fn is_pipe() -> bool {
     !atty::is(Stream::Stdin)
+}
+
+// generate filename from date/time
+fn get_new_filename() -> String {
+    let now: DateTime<Utc> = Utc::now();
+    now.format("%y%m%d%H%M.md").to_string()
 }
